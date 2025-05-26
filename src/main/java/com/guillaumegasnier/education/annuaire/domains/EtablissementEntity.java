ce901642272f4f03430c751ba977439bc2f83db6
ggasnier <23357977+ggasnier@users.noreply.github.com>
2025-05-26 09:35:18 +0200

    feature(etablissement): Première structure

--- src/main/java/com/guillaumegasnier/education/annuaire/domains/EtablissementEntity.java
+++ src/main/java/com/guillaumegasnier/education/annuaire/domains/EtablissementEntity.java
@@ -1,9 +1,11 @@
 package com.guillaumegasnier.education.annuaire.domains;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.guillaumegasnier.education.annuaire.enums.EtatEtablissement;
+import com.guillaumegasnier.education.annuaire.validations.ValidSiret;
+import com.guillaumegasnier.education.annuaire.validations.ValidUai;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +16,36 @@ import lombok.Setter;
 public class EtablissementEntity {
 
     @Id
+    @ValidUai
     @Column(columnDefinition = "CHAR(8)", unique = true, updatable = false, nullable = false, length = 8)
     private String uai;
+
+    @ValidSiret
+    @Column(columnDefinition = "CHAR(14)", length = 14)
+    private String siret;
+
+    @NotBlank
+    private String nom;
+
+    @NotNull
+    @Column(columnDefinition = "CHAR(1)", length = 1, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EtatEtablissement etat;
+
+    @ManyToOne
+    @JoinColumn(name = "nature_code")
+    private NatureEntity nature;
+
+    private String adresse;
+
+    private String complement;
+
+    private String codePostal;
+
+    @ManyToOne
+    @JoinColumn(name = "commune_code")
+    private CommuneEntity commune;
+
+    // Position (POSTGIS)
+
 }
