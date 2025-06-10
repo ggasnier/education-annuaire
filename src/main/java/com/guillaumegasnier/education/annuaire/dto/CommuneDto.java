18b5a594866ae404c3dda69e358345c4809f6893
Guillaume Gasnier <23357977+ggasnier@users.noreply.github.com>
2025-06-10 08:57:24 +0000

    feature(etablissement): Ajout du détails de l'établissement

--- src/main/java/com/guillaumegasnier/education/annuaire/dto/CommuneDto.java
+++ src/main/java/com/guillaumegasnier/education/annuaire/dto/CommuneDto.java
@@ -1,5 +1,6 @@
 package com.guillaumegasnier.education.annuaire.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
