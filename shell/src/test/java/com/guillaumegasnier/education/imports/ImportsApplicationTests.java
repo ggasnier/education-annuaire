f7325b447574677dbbe8ea40b15d3fc840e759f1
ggasnier <contact@guillaumegas,ier.com>
2025-07-21 18:31:56 +0200

    feature(core): Passage du projet en modules

--- shell/src/test/java/com/guillaumegasnier/education/imports/ImportsApplicationTests.java
+++ shell/src/test/java/com/guillaumegasnier/education/imports/ImportsApplicationTests.java
@@ -1,12 +1,7 @@
 package com.guillaumegasnier.education.imports;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ImportsApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
