6f12cff22cc7b6efc301f76c17e887cc43baf0fd
ggasnier <contact@guillaumegasnier.com>
2026-02-04 13:11:09 +0100

    feature(tests): Ajouts de tests pour le shell

--- shell/src/test/java/com/guillaumegasnier/education/shell/transformers/impl/FormationTransformerImplTest.java
+++ shell/src/test/java/com/guillaumegasnier/education/shell/transformers/impl/FormationTransformerImplTest.java
@@ -1,10 +1,12 @@
 package com.guillaumegasnier.education.shell.transformers.impl;
 
-import com.guillaumegasnier.education.core.domains.formations.LienOnisepEntity;
-import com.guillaumegasnier.education.core.domains.formations.LienOnisepPK;
+import com.guillaumegasnier.education.core.domains.etablissements.EtablissementEntity;
+import com.guillaumegasnier.education.core.domains.formations.*;
 import com.guillaumegasnier.education.core.services.CoreEtablissementService;
 import com.guillaumegasnier.education.core.services.CoreFormationService;
 import com.guillaumegasnier.education.core.services.CoreTerritoireService;
+import com.guillaumegasnier.education.shell.datasets.etablissements.TravailOrganismeFormationDataset;
+import com.guillaumegasnier.education.shell.dto.formations.ActionFormationDTO;
 import com.guillaumegasnier.education.shell.dto.formations.FormationDTO;
 import com.guillaumegasnier.education.shell.mappers.EtablissementMapper;
 import com.guillaumegasnier.education.shell.mappers.FormationMapper;
@@ -17,17 +19,19 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 class FormationTransformerImplTest {
 
+    String uaiExiste = "0750683K";
+    String uaiAbsent = "0750652B";
     @Mock
     private CoreTerritoireService coreTerritoireService;
     @Mock
     private CoreFormationService coreFormationService;
     @Mock
     private CoreEtablissementService coreEtablissementService;
-
     private FormationTransformerImpl transformer;
 
     @BeforeEach
@@ -39,7 +43,7 @@ class FormationTransformerImplTest {
     }
 
     @Test
-    void recalculId() {
+    void recalculIdTest() {
         FormationDTO dto = new FormationDTO();
         dto.setFormationId(1L);
         dto.setParcoursupId(null);
@@ -66,13 +70,55 @@ class FormationTransformerImplTest {
 
     @Test
     void toOrganismeEntity() {
+        TravailOrganismeFormationDataset dataset = new TravailOrganismeFormationDataset();
+        dataset.setDateDebut("2026-01-01");
+        dataset.setDateFin("2026-01-02");
+        dataset.setDateDerniereDeclaration("2026-01-03");
+        assertNotNull(transformer.toOrganismeEntity(dataset));
+
+        dataset.setNumeroDeclarationActivite("123456");
+
+        var entity = new OrganismeEntity();
+
+        when(coreEtablissementService.findOrganisme("123456")).thenReturn(Optional.of(entity));
+        assertNotNull(transformer.toOrganismeEntity(dataset));
+
     }
 
     @Test
     void toFormationEntity() {
+        FormationDTO dto = new FormationDTO();
+        assertNotNull(transformer.toFormationEntity(dto, "test"));
+
+        dto.setFormationId(123L);
+        when(coreFormationService.findFormation(123L)).thenReturn(Optional.of(new FormationEntity()));
+        assertNotNull(transformer.toFormationEntity(dto, "test"));
+
     }
 
     @Test
     void toActionFormationEntity() {
+
+        when(coreFormationService.findActionFormation(456L)).thenReturn(Optional.empty());
+        when(coreFormationService.findActionFormation(789L)).thenReturn(Optional.of(new ActionFormationEntity()));
+        when(coreEtablissementService.isEtablissementExiste(uaiExiste)).thenReturn(true);
+        when(coreEtablissementService.isEtablissementExiste(uaiAbsent)).thenReturn(false);
+        when(coreEtablissementService.getEtablissementReferenceByUai(uaiExiste)).thenReturn(new EtablissementEntity());
+
+        ActionFormationDTO dto = new ActionFormationDTO();
+        assertNotNull(transformer.toActionFormationEntity(dto, "test"));
+
+        dto.setActionFormationId(456L);
+        dto.setUai("");
+        assertNotNull(transformer.toActionFormationEntity(dto, "test"));
+
+        dto.setUai(uaiExiste);
+        assertNotNull(transformer.toActionFormationEntity(dto, "test"));
+
+        dto.setUai(uaiAbsent);
+        assertNotNull(transformer.toActionFormationEntity(dto, "test"));
+
+        dto.setActionFormationId(789L);
+        assertNotNull(transformer.toActionFormationEntity(dto, "test"));
     }
 }
